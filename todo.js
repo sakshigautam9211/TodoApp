86ee39dcb9ecdb1ead17c1f8c656b1214cf79ea0
sakshigautam9211 <singhraj38032@gmail.com>
2025-09-25 15:57:16 +0530

    Add edit button

--- todo.js
+++ todo.js
@@ -1,30 +1,27 @@
-let todoList = [
-  { item: 'Buy Milk', dueDate: '4/10/2023' },
-  { item: 'Go to College', dueDate: '4/10/2023' }
-];
+let todoList = [];
 
 displayItems();
 
 function addTodo() {
-  let inputElement = document.querySelector('#input');
-  let dateElement = document.querySelector('#date');
+  let inputElement = document.querySelector("#input");
+  let dateElement = document.querySelector("#date");
   let todoItem = inputElement.value.trim();
   let todoDate = dateElement.value;
 
-  if (todoItem === '') {
+  if (todoItem === "") {
     alert("Please enter a todo!");
     return;
   }
 
   todoList.push({ item: todoItem, dueDate: todoDate });
-  inputElement.value = '';
-  dateElement.value = '';
+  inputElement.value = "";
+  dateElement.value = "";
   displayItems();
 }
 
 function displayItems() {
-  let listElement = document.querySelector('.lists');
-  let newHtml = '';
+  let listElement = document.querySelector(".lists");
+  let newHtml = "";
 
   for (let i = 0; i < todoList.length; i++) {
     let { item, dueDate } = todoList[i];
@@ -34,13 +31,14 @@ function displayItems() {
         <span>${item}</span>
         <span>${dueDate}</span>
         <div class="buttons">
+          <button onclick="editTodo(${i})">Edit</button>
           <button onclick="deleteTodo(${i})">Delete</button>
         </div>
       </div>
     `;
   }
 
-  // Update once after loop finishes
+  // Update the DOM once after loop
   listElement.innerHTML = newHtml;
 }
 
@@ -49,3 +47,22 @@ function deleteTodo(index) {
   displayItems();
 }
 
+
+function editTodo(index) {
+  let currentTodo = todoList[index];
+
+  
+  let updatedItem = prompt("Edit your todo:", currentTodo.item);
+  let updatedDate = prompt("Edit due date (YYYY-MM-DD):", currentTodo.dueDate);
+
+  
+  if (updatedItem !== null && updatedItem.trim() !== "") {
+    currentTodo.item = updatedItem.trim();
+  }
+
+  if (updatedDate !== null && updatedDate.trim() !== "") {
+    currentTodo.dueDate = updatedDate.trim();
+  }
+
+  displayItems();
+}
